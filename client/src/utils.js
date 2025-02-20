ef50521ec3fc6919dc5ee2ae0a279e64e17b794c
hugh long <hal2209122@gmail.com>
2025-02-20 14:21:04 -0500

    minor html

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,3 +1,4 @@
-export function onLoad(){ //simply to render tailwind componeents visible after the rendering completes
+export function onLoad(){ //simply to render tailwind componeents visible after the rendering completes , stop event propogationfullout on elements, etc..
     document.getElementById('search-bar').style.visibility = 'visible';
-}
+    document.getElementById('search-bar').addEventListener('submit' , function (event){event.stopPropagation();})
+}    
