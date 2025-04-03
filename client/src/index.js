c09854294d0d56d38c1455846ee0aa7b46a8fcd6
Best-code <cpmraygun@gmail.com>
2025-04-03 12:50:06 -0400

    Selected layers are easily identified - Except public lands i don't know why

--- client/src/index.js
+++ client/src/index.js
@@ -1,122 +1,186 @@
 import 'leaflet'
-import { layerCarry , getViirs, getWfigs, handleSmallLayer, initialize_map, handleFlPublicTiles, handlePrivateTiles, addLocationMarker , getLocationIcon , removeLocationMarker} from "./utilities/map_utils.js"
+import { layerCarry, getViirs, getWfigs, handleSmallLayer, initialize_map, handleFlPublicTiles, handlePrivateTiles, addLocationMarker, getLocationIcon, removeLocationMarker } from "./utilities/map_utils.js"
 import './styles.css'
-import { geoWrap , getUserCoords, onLoad, handleSearch, } from './utilities/utils.js'
+import { geoWrap, getUserCoords, onLoad, handleSearch, } from './utilities/utils.js'
 
 //eliminates flashing of unstyled components -- could use SSR to fix but this works
 window.onload = onLoad;
 
-const mapBounds = L.latLngBounds([[-20 , 0], [ 90,-180]]); //use for us mapbounds
+const mapBounds = L.latLngBounds([[-20, 0], [90, -180]]); //use for us mapbounds
 const minZoom = 4;
-const zoomStart = 4;   
+const zoomStart = 4;
 const apiUrl = 'http://localhost:4242'; //adjust   
-const defCenter = [32,-80]; //about the cente rof the us, starting value if geoloc is declined
+const defCenter = [32, -80]; //about the cente rof the us, starting value if geoloc is declined
 
-const locationIcon = new layerCarry(getLocationIcon([40,40])); //consistent style across iterations
+const locationIcon = new layerCarry(getLocationIcon([40, 40])); //consistent style across iterations
 
 
 /*this global will change based on whatever location marker is currently placed on the map , 
 defaulting to wherever the userlocation is , or, if null then nowhere*/
-var locationMarker = new layerCarry(null); 
+var locationMarker = new layerCarry(null);
 
 //map instance with satelite baselayer and appropriate bounds
 
-document.getElementById('viirs-desc').innerText = 'Possible Wildfires';
-document.getElementById('wfigs-desc').innerText = 'Current Wildfires';
-document.getElementById('fl-conserve-desc').innerText = 'FL Public Lands';
-document.getElementById('fl-private-desc').innerText = 'FL Private Lands';
+var viirsDesc = document.getElementById('viirs-desc');
+var wfigsDesc = document.getElementById('wfigs-desc');
+var flConserveDesc = document.getElementById('fl-conserve-desc');
+var flPrivateDesc = document.getElementById('fl-private-desc');
 
-const satMap = initialize_map('map' , 
-    'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}' , 
+viirsDesc.innerText = 'Possible Wildfires';
+wfigsDesc.innerText = 'Current Wildfires';
+flConserveDesc.innerText = 'FL Public Lands';
+flPrivateDesc.innerText = 'FL Private Lands';
+
+var viirs = document.getElementById('viirs');
+var wfigs = document.getElementById('wfigs');
+var flConserve = document.getElementById('flConserve');
+var flPrivate = document.getElementById('fl-private');
+
+
+const satMap = initialize_map('map',
+    'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
     'Tiles &copy; Esri and the GIS User Community',
-    minZoom, 
+    minZoom,
     mapBounds,
     zoomStart,
     defCenter
 );
 
-const transitMap = initialize_map(null, 
+const transitMap = initialize_map(null,
     'https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}',
     'Tiles &copy; Esri and the GIS User Community'
 );
 
-const map = satMap.map;  
+const map = satMap.map;
 const esriTiles = satMap.tiles;
 const transitTiles = transitMap.tiles;
 
 esriTiles.addTo(map); //adds satelite base to map as the default
 
-map.on('dragstart' , ()=>{document.getElementById('search-input').value = ''; var results = document.getElementsByClassName('search-result'); while(results.length > 0){results[0].remove();}});
+map.on('dragstart', () => { document.getElementById('search-input').value = ''; var results = document.getElementsByClassName('search-result'); while (results.length > 0) { results[0].remove(); } });
 
 //sets map to initial user locaion and allows for snapping to user position from navbar
-map.on('locationfound' , (locationEvent)=>{map.setView(locationEvent.latlng , 11); removeLocationMarker(map, locationMarker.obj); addLocationMarker(map , locationIcon.obj , locationMarker , locationEvent.latlng);});
+map.on('locationfound', (locationEvent) => { map.setView(locationEvent.latlng, 11); removeLocationMarker(map, locationMarker.obj); addLocationMarker(map, locationIcon.obj, locationMarker, locationEvent.latlng); });
 
 //hoestly may drop the below section to just use the leaflet api instead of making a new request
 var userLocation = new geoWrap(null);
 
-map.locate({maximumAge : 600000}); //sets map to userlocation if approved
+map.locate({ maximumAge: 600000 }); //sets map to userlocation if approved
 
 //try and get user location 
 getUserCoords(userLocation);
 
 //get geolocal search results if possible
-document.getElementById('search-input').addEventListener("keystopped" , ()=>{
-    if(userLocation.obj instanceof GeolocationPosition){handleSearch(userLocation.obj.coords.latitude , userLocation.obj.coords.longitude , apiUrl , map ,  locationMarker , locationIcon.obj)}
-    else{const center = map.getCenter();  handleSearch(center.lat , center.lng , apiUrl , map , locationMarker , locationIcon.obj)}});
+document.getElementById('search-input').addEventListener("keystopped", () => {
+    if (userLocation.obj instanceof GeolocationPosition) { handleSearch(userLocation.obj.coords.latitude, userLocation.obj.coords.longitude, apiUrl, map, locationMarker, locationIcon.obj) }
+    else { const center = map.getCenter(); handleSearch(center.lat, center.lng, apiUrl, map, locationMarker, locationIcon.obj) }
+});
 
 
 //navbar events
-document.getElementById('zoom-in').addEventListener('click' , ()=>{map.zoomIn(1);});
-document.getElementById('zoom-out').addEventListener('click' , ()=>{map.zoomOut(1);});
-document.getElementById('locate-me').addEventListener('click' , ()=>{map.locate(600000);});
+document.getElementById('zoom-in').addEventListener('click', () => { map.zoomIn(1); });
+document.getElementById('zoom-out').addEventListener('click', () => { map.zoomOut(1); });
+document.getElementById('locate-me').addEventListener('click', () => { map.locate(600000); });
 //end navbar events
 
 //loaded smaller geojson based layers on page load but not placed on map
 var viirsLayer = null;
 var wfigsLayer = null;
 
-try{
+try {
     viirsLayer = await getViirs(apiUrl);
-}catch(error){}
+} catch (error) { }
 
-try{
+try {
     wfigsLayer = await getWfigs(apiUrl);
-}catch(error){}
+} catch (error) { }
 
 //deals with passing stuff to js fuctions, we need the layer saved in the global scope so we can remove, which
 //requires it be mutatable by functions, ergo the wrapper class
-var flConserve = new layerCarry(null); 
+var flConserve = new layerCarry(null);
 var privateLands = new layerCarry(null);
 
 
-document.getElementById('fl-conserve').addEventListener('click' , ()=>{handleFlPublicTiles(map , flConserve , apiUrl);});
-document.getElementById('viirs').addEventListener('click', ()=>{handleSmallLayer(map ,viirsLayer);});   
-document.getElementById('wfigs').addEventListener('click' , ()=>{handleSmallLayer(map , wfigsLayer)});
-document.getElementById('fl-private').addEventListener('click' , ()=>{handlePrivateTiles(map , privateLands , apiUrl)});
+document.getElementById('fl-conserve').addEventListener('click', () => {
+    const currentTransform = flConserve.style.transform;
+
+    if (currentTransform === 'scale(1)') {
+        flConserve.style.transform = 'scale(1.1)';
+        flConserve.style.color = "orange";
+    } else {
+        flConserve.style.transform = 'scale(1)';
+        flConserve.style.color = "white";
+    }
+
+    handleFlPublicTiles(map, flConserve, apiUrl);
+
+});
+document.getElementById('viirs').addEventListener('click', () => {
+    handleSmallLayer(map, viirsLayer);
+    const currentTransform = viirs.style.transform;
+
+    if (currentTransform === 'scale(1)') {
+        viirs.style.transform = 'scale(1.1)';
+        viirs.style.color = "orange";
+    } else {
+        viirs.style.transform = 'scale(1)';
+        viirs.style.color = "white";
+    }
+
+});
+document.getElementById('wfigs').addEventListener('click', () => {
+    handleSmallLayer(map, wfigsLayer)
+    const currentTransform = wfigs.style.transform;
+
+    if (currentTransform === 'scale(1)') {
+        wfigs.style.transform = 'scale(1.1)';
+        wfigs.style.color = "orange";
+    } else {
+        wfigs.style.transform = 'scale(1)';
+        wfigs.style.color = "white";
+    }
+});
+document.getElementById('fl-private').addEventListener('click', () => {
+    handlePrivateTiles(map, privateLands, apiUrl)
+    const currentTransform = flPrivate.style.transform;
+
+    if (currentTransform === 'scale(1)') {
+        flPrivate.style.transform = 'scale(1.1)';
+        flPrivate.style.color = "orange";
+    } else {
+        flPrivate.style.transform = 'scale(1)';
+        flPrivate.style.color = "white";
+    }
+
+});
+
 // document.getElementById('result-popup-close').addEventListener('click' , ()=>{
 //     // var infoBox = document.getElementById('result-info-popup'); 
 //     // infoBox.style.height = 0; infoBox.style.visibility = 'hidden';
 //      if(locationMarker.obj instanceof L.Marker){removeLocationMarker(map , locationMarker.obj);}});
-document.querySelectorAll('.basemap').forEach( element => {element.addEventListener('click' , (event)=>{
 
-    var selectors = document.getElementsByClassName('basemap');
+document.querySelectorAll('.basemap').forEach(element => {
+    element.addEventListener('click', (event) => {
 
-    for(var i = 0 ; i < selectors.length ; i++){
+        var selectors = document.getElementsByClassName('basemap');
 
-        if(selectors[i].checked){
-            selectors[i].checked = false;
+        for (var i = 0; i < selectors.length; i++) {
+
+            if (selectors[i].checked) {
+                selectors[i].checked = false;
+            }
         }
-    }
 
-    event.target.checked = true;
+        event.target.checked = true;
 
-    if(event.target.id == 'satellite'){
-        map.removeLayer(transitTiles);
-        map.addLayer(esriTiles);
-    }
-    else if(event.target.id == 'streets'){
-        map.removeLayer(esriTiles);
-        map.addLayer(transitTiles);
-    }
+        if (event.target.id == 'satellite') {
+            map.removeLayer(transitTiles);
+            map.addLayer(esriTiles);
+        }
+        else if (event.target.id == 'streets') {
+            map.removeLayer(esriTiles);
+            map.addLayer(transitTiles);
+        }
 
-})});
+    })
+});
